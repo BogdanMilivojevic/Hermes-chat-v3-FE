f947ad0acf871ceba6e9c091a0651ef610e67fa3
BogdanMilivojevic <111180971+BogdanMilivojevic@users.noreply.github.com>
2023-08-31 20:23:47 +0200

    merge-login-form (#1)

--- app/page.tsx
+++ app/page.tsx
@@ -2,21 +2,23 @@
 
 import Link from 'next/link';
 import { motion } from 'framer-motion'
+import { useState } from 'react';
 
 
 export default function HermesChat() {
+    const [heading,setHeading] = useState()
+
     return( 
         <motion.h1 className='homepage-cta'  
-            initial={{    x:'-120%', y: '-50%'}}
+            initial={{    x:'-125%', y: '-50%', opacity:0.20}}
             animate={{
                 x:'0%',
                 y:'0%',
                 translateX: '-50%',
                 translateY: '-50%',
+                opacity:1
             }}
             transition={{ duration: 0.5, ease: 'easeOut' }}
-        >
-                Hello there, 
-            <Link className='homepage-cta-link' href='/login'>login</Link> to start chatting</motion.h1>
+        >  <Link className='homepage-cta-link' href='/login'>Login</Link>to start chatting</motion.h1>
     )
 }
