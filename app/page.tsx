fd78dd6f75c278ea9aae7225a6a015e162e74be6
BogdanMilivojevic <111180971+BogdanMilivojevic@users.noreply.github.com>
2023-09-20 13:08:41 +0200

    register-form-merge (#2)

--- app/page.tsx
+++ app/page.tsx
@@ -9,16 +9,18 @@ export default function HermesChat() {
     const [heading,setHeading] = useState()
 
     return( 
-        <motion.h1 className='homepage-cta'  
-            initial={{    x:'-125%', y: '-50%', opacity:0.20}}
-            animate={{
-                x:'0%',
-                y:'0%',
-                translateX: '-50%',
-                translateY: '-50%',
-                opacity:1
-            }}
-            transition={{ duration: 0.5, ease: 'easeOut' }}
-        >  <Link className='homepage-cta-link' href='/login'>Login</Link>to start chatting</motion.h1>
+        <>
+            <motion.h1 className='homepage-cta'  
+                initial={{    x:'-125%', y: '-50%', opacity:0.20}}
+                animate={{
+                    x:'0%',
+                    y:'0%',
+                    translateX: '-50%',
+                    translateY: '-50%',
+                    opacity:1
+                }}
+                transition={{ duration: 0.5, ease: 'easeOut' }}
+            >  <Link className='homepage-cta-link' href='/login'>Login</Link>to start chatting</motion.h1>
+        </>
     )
 }
